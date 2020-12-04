f9e70d515b8c85f34ccce9b38ae410a896e0e1fc
Kohei Terashita <kou0704terasita@gmail.com>
2020-12-05 00:12:36 +0900

    自動整形した

--- pages/likes.vue
+++ pages/likes.vue
@@ -2,16 +2,14 @@
   <v-app>
     <v-container class="primary">
       <v-row>
-        <v-col cols="12" md="6"
-        v-for="like in likes"
-        :key="like.id">
+        <v-col cols="12" md="6" v-for="like in likes" :key="like.id">
           <v-card>
             <v-row>
-              <v-col cols="2"
-              align-self="center">
-                <v-img :src="like.picture"
-                class="member-picture"
-                width="100%"
+              <v-col cols="2" align-self="center">
+                <v-img
+                  :src="like.picture"
+                  class="member-picture"
+                  width="100%"
                 />
               </v-col>
               <v-col cols="6">
@@ -22,46 +20,45 @@
                   {{ like.createdAt }}
                 </v-card-subtitle>
               </v-col>
-              <v-col cols="3"
-              align-self="center"
-              justify="center">
-              </v-col>
+              <v-col cols="3" align-self="center" justify="center"> </v-col>
               <v-col cols="1"></v-col>
             </v-row>
           </v-card>
-
         </v-col>
       </v-row>
     </v-container>
   </v-app>
-  
 </template>
 
 <script>
-import LikeButton from '~/components/LikeButton.vue'
-import ProfileButton from '~/components/ProfileButton.vue'
+import LikeButton from "~/components/LikeButton.vue";
+import ProfileButton from "~/components/ProfileButton.vue";
 export default {
-  layout: 'navbar',
+  layout: "navbar",
   components: {
     LikeButton,
     ProfileButton,
   },
-  data () {
+  data() {
     return {
       currentUser: {},
       likes: [],
-    }
+    };
   },
   created() {
-    this.$fireAuth.onAuthStateChanged(user => {
-      this.$firestore.collection('profiles').doc(user.uid)
-      .collection('likedProfileUsers')
-      .get().then((querySnapshot) => {
-        this.likes = querySnapshot.docs.map(doc => doc.data())
-      })
-    })
-  }
-}
+    this.$fireAuth.onAuthStateChanged((user) => {
+      this.$firestore
+        .collection("profiles")
+        .doc(user.uid)
+        .collection("likedProfileUsers")
+        .get()
+        .then((querySnapshot) => {
+          this.likes = querySnapshot.docs.map((doc) => doc.data());
+          console.log(this.likes);
+        });
+    });
+  },
+};
 </script>
 
 <style>
