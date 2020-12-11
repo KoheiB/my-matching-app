98fd6e111ca16764a158ec1eb4f401dc34272b46
Kohei Terashita <kou0704terasita@gmail.com>
2020-12-11 22:33:56 +0900

    updated messageview

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const https = functions.region("asia-northeast1").https;
+// const https = functions.region("asia-northeast1").https;
 const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
@@ -11,15 +11,19 @@ exports.createChatroom = functions.firestore.document('profiles/{profileId}/like
   const newData = newSnapshot.data();
   const isApproved = newData.isApproved
   const likedUserRef = newData.likedUserRef
-
+  const userId = context.params.profileId
+  
   if (!isApproved) {
     return null;
   }
-
-  db.collection('chatrooms').doc()
+  
+  db
+  .collection('users').doc(userId)
+  .collection('chatrooms').doc()
   .set({
     attendUserRef: likedUserRef,
     updatedAt: admin.firestore.FieldValue.serverTimestamp()
   }, {merge: true});
+
   return null;
 })
--- pages/messages/index.vue
+++ pages/messages/index.vue
@@ -3,14 +3,15 @@
     <v-container class="primary">
       <v-row>
         <v-col>
-
+          <!-- {{ chatrooms }} -->
         </v-col>
       </v-row>
       <v-card
         v-for="chatroom in chatrooms"
-        :key="chatroom.id">
+        :key="chatroom.id"
+        >
         <v-card-title>
-          {{ room.name }}
+          {{ chatroom.updatedAt }}
         </v-card-title>
         <v-card-text>
           content
@@ -25,9 +26,33 @@ export default {
   layout: 'navbar',
   data () {
     return {
-      chatrooms: []
+      chatrooms: [],
+      currentUser: {},
     }
-  }
+  },
+  created() {
+    this.$fireAuth.onAuthStateChanged(async(user) => {
+      this.currentUser = user
+      try {
+        const querySnapshot = await this.$firestore
+          .collection("users")
+          .doc(user.uid)
+          .collection("chatrooms")
+          .get()
+        const chatrooms = querySnapshot.docs.map((doc) => {
+          const result = doc.data()
+          result.id = doc.id
+          return result
+        });
+        // console.log(chatrooms);
+        chatrooms.forEach((chatroom) => {
+          this.chatrooms.push(chatroom)
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+  },
 }
 </script>
 
